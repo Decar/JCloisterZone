67066445a0768a2e558968176f06dd3f8eaacd39
Tom Hill <tehill+git@gmail.com>
2014-12-20 18:10:11 +0000

    Replaced screenshot F2 with F12

--- src/main/java/com/jcloisterzone/ui/MenuBar.java
+++ src/main/java/com/jcloisterzone/ui/MenuBar.java
@@ -145,7 +145,7 @@ public class MenuBar extends JMenuBar {
         menu.addSeparator();
         
         screenShot = new JMenuItem(_("Take Screenshot"));
-        screenShot.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F2,0));
+        screenShot.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F12,0));
         screenShot.setEnabled(false);
         screenShot.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
