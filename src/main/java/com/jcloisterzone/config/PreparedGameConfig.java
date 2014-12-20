efc669fba95e1eb2b37c0b73b8e5a7a3281dce5b
Tom Hill <tehill+git@gmail.com>
2014-12-20 11:19:08 +0000

    Removed Tabs from PreparedGameConfig
    Removed Tabs from PreparedGameConfig file

--- src/main/java/com/jcloisterzone/config/PreparedGameConfig.java
+++ src/main/java/com/jcloisterzone/config/PreparedGameConfig.java
@@ -4,18 +4,18 @@ import java.util.List;
 
 public class PreparedGameConfig {
 
-	private List<String> draw;
-	public List<String> getDraw() {
-		return draw;
-	}
-	public void setDraw(List<String> draw) {
-		this.draw = draw;
-	}
-	public int totalSize() {
-		int endindex = draw.indexOf(".");
-		if (endindex == -1){
-			return draw.size();
-		}
-		return endindex;
-	}    	
+    private List<String> draw;
+    public List<String> getDraw() {
+        return draw;
+    }
+    public void setDraw(List<String> draw) {
+        this.draw = draw;
+    }
+    public int totalSize() {
+        int endindex = draw.indexOf(".");
+        if (endindex == -1){
+            return draw.size();
+        }
+        return endindex;
+    }
 }
